02382d8a6c40b054a6026abac1a5171d3be001fd
Andrei <nestorivanov163@gmail.com>
2023-10-23 18:55:04 +0400

    DZ44

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -1,5 +1,33 @@
+package Calculator;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое целое число: ");
+        int a = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Введите второе целое число: ");
+        int b = scanner.nextInt();
+        System.out.print("Введите число от 1 до 4: ");
+        int f = scanner.nextInt();
+
+        if (f == 1) {
+            int c1 = a + b;
+            System.out.println("Сумма : " + c1);
+        } else if (f == 2) {
+            int c2 = a - b;
+            System.out.println("Разность : " + c2);
+        } else if (f == 3) {
+            int c3 = a * b;
+            System.out.println("Произведение : " + c3);
+        } else{
+            int c4 = a / b;
+            System.out.println("Частное : " + c4);
+        }
+
+
     }
 }
--- src/Massiv/Main.java
+++ src/Massiv/Main.java
@@ -1,2 +1,48 @@
-package Massiv;public class Main {
+package Massiv;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[50];
+        Random random = new Random();
+
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(101) - 50;
+        }
+
+
+        int colOtrElem = 0, colPolElem = 0, colNull = 0, sumPolElem = 0, sumOtrElem = 0, sum = 0;
+
+
+        for (int num : array) {
+
+            if (num > 0) {
+                colPolElem++;
+                sumPolElem += num;
+            }
+
+            else if (num < 0) {
+                colOtrElem++;
+                sumOtrElem += num;
+            }
+
+            else {
+                colNull++;
+            }
+
+
+            sum += num;
+        }
+
+        double sredZnachMass = (double) sum / array.length;
+
+        System.out.println("Количество положительных элементов: " + colPolElem);
+        System.out.println("Количество отрицательных элементов: " + colOtrElem);
+        System.out.println("Количество нулей: " + colNull);
+        System.out.println("Сумма положительных элементов: " + sumPolElem);
+        System.out.println("Сумма отрицательных элементов: " + sumOtrElem);
+        System.out.println("Среднее значение элементов массива: " + sredZnachMass);
+    }
 }
